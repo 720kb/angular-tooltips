52099222118f1b737af6da20d96925ef6839ccea
wouldgo <wouldgo84@gmail.com>
2015-10-27 22:24:05 +0100

    polishing

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,6 @@
   'use strict';
 
   angular.module('720kb', [
-    'ngRoute',
     '720kb.tooltips'
   ])
   .controller('Ctrl', [
@@ -12,14 +11,16 @@
     '$timeout',
     function controllerCtrl($scope, $timeout) {
 
-    $scope.generateHTMLextra = function generateHTMLextra(item) {
+      $scope.generateHTMLextra = function generateHTMLextra(item) {
 
-      return '<i>hello tooltip content' + item + '</i>';
-    };
-    $scope.items = ['1','2','4','5'];
-    $timeout(function () {
-      $scope.items.push('7');
+        return '<i>hello tooltip content' + item + '</i>';
+      };
+      $scope.items = ['1','2','4','5'];
+      $timeout(function doTimeout() {
+
+        $scope.items.push('7');
         $scope.items.push('9');
-    }, 5000);
-  }]);
+      }, 5000);
+    }
+    ]);
 }(angular));
