27458c7b16626ba1ab0ce45dda0312b9fa58bd7e
wouldgo <wouldgo84@gmail.com>
2015-10-27 22:16:38 +0100

    first step

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*global require,module*/
+(function commonJS(require, module) {
+  'use strict';
+
+  require('./src/js/angular-tooltips');
+
+  module.exports = '720kb.tooltips';
+}(require, module));
