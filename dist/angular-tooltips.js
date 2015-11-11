99bdc9e6b05ab54baafb1a398739ef3e69ef5214
wouldgo <wouldgo84@gmail.com>
2015-11-12 00:47:27 +0100

    positioning support multiline for tooltipped content, now multiline tooltip is the step

--- dist/angular-tooltips.js
+++ dist/angular-tooltips.js
@@ -1,27 +1,69 @@
-/*global angular,document*/
-(function withAngular(angular, document) {
+/*global angular,window*/
+(function withAngular(angular, window) {
   'use strict';
 
   var directiveName = 'tooltips'
   , marginLeftTooltipArrow = 8
   , deltaTooltipFromTooltipContent = 2
+  , resizeObserver = (function optimizedResize() {
+
+    var callbacks = []
+      , running = false
+      , runCallbacks = function runCallbacks() {
+
+        callbacks.forEach(function iterator(callback) {
+
+          callback();
+        });
+        running = false;
+      }
+      , resize = function resize() {
+
+        if (!running) {
+
+          running = true;
+          if (window.requestAnimationFrame) {
+
+            window.requestAnimationFrame(runCallbacks);
+          } else {
+
+            window.setTimeout(runCallbacks, 66);
+          }
+        }
+      }
+      , addCallback = function addCallback(callback) {
+
+        if (callback) {
+
+          callbacks.push(callback);
+        }
+      };
+
+    return {
+      'add': function add(callback) {
+
+        if (!callbacks.length) {
+
+          window.addEventListener('resize', resize);
+        }
+        addCallback(callback);
+      }
+    };
+  }())
   , getElementHTML = function getElementHTML(element) {
     var txt
-      , ax
-      , el = document.createElement('div');
+      , el = window.document.createElement('div');
 
     element.removeAttr(directiveName);
     el.appendChild(element.clone()[0]);
     txt = el.innerHTML;
-    ax = txt.indexOf('>') + 1;
-    txt = txt.substring(0, ax) + element.html() + txt.substring(ax);
     el = null;
     return txt;
   }
-  , trasformTooltipContent = function trasformTooltipContent(tooltippedContent, tooltipContent, tooltipTemplate, side) {
+  , trasformTooltipContent = function trasformTooltipContent(tooltippedContent, tooltipTemplate) {
 
     var toReturn = [
-      '<a tooltips title="' + tooltipContent + '" class="' + side + ' tooltips">',
+      '<tooltip tooltips class="tooltips">',
         '<tip-cont>',
           tooltippedContent,
         '</tip-cont>'
@@ -35,12 +77,12 @@
         '</tip>'
       ]);
     }
-    toReturn.push('</a>');
+    toReturn.push('</tooltip>');
     return toReturn.join(' ');
   }
   , TooltipController = /*@ngInject*/ ["$log", function TooltipController($log) {
 
-    $log.info('controller called');
+    $log.debug('controller called');
   }]
   , tooltipDirective = /*@ngInject*/ function tooltipDirective() {
 
@@ -61,69 +103,111 @@
       'controller': TooltipController,
       'compile': function compiling(compileElement, compileAttributes) {
 
-        compileAttributes.tooltipTitle = compileAttributes.tooltipTitle || '';
         compileAttributes.tooltipTemplate = compileAttributes.tooltipTemplate || '';
         compileAttributes.tooltipSide = compileAttributes.tooltipSide || 'top';
         compileAttributes.tooltipShowTrigger = compileAttributes.tooltipShowTrigger || 'mousemove';
         compileAttributes.tooltipHideTrigger = compileAttributes.tooltipHideTrigger || 'mouseout';
         var initialElement = getElementHTML(compileElement)
           , startingTooltipContent = trasformTooltipContent(initialElement,
-            compileAttributes.tooltipTitle,
-            compileAttributes.tooltipTemplate,
-            compileAttributes.tooltipSide);
+            compileAttributes.tooltipTemplate);
 
         compileElement.replaceWith(startingTooltipContent);
         return function linkingFunction(scope, element, attrs) {
 
-          var onTooltipShow = function onTooltipShow() {
+          var onTooltipShow = function onTooltipShow(event) {
             var tipContElement = element.find('tip-cont')
               , tipElement = element.find('tip')
+              , theTipContElement
+              , theTipElement
+              , theTipContClientRects
+              , theTipClientRects
+              , clientRectIndex
+              , aClientRect
+              , contentMinLeft = Number.MAX_SAFE_INTEGER
               , newLeft
               , newTop;
 
-            element.addClass('active');
+            if (event) {
+
+              element.addClass('active');
+            }
             if (tipContElement.length > 0 &&
               tipElement.length > 0) {
 
-              if (attrs.tooltipSide === 'top') {
+              theTipContElement = tipContElement[0];
+              theTipElement = tipElement[0];
+              theTipContClientRects = theTipContElement.getClientRects();
+              theTipClientRects = theTipElement.getClientRects();
+              if (theTipClientRects.length > 0) {
 
-                newLeft = tipContElement[0].offsetLeft + tipContElement[0].offsetWidth / 2 - tipElement[0].offsetWidth / 2;
-                newTop = tipContElement[0].offsetTop - marginLeftTooltipArrow - tipElement[0].offsetHeight - deltaTooltipFromTooltipContent;
-              } else if (attrs.tooltipSide === 'left') {
+                /*jshint -W014*/
+                if (attrs.tooltipSide === 'top') {
 
-                newLeft = tipContElement[0].offsetLeft - tipElement[0].offsetWidth - marginLeftTooltipArrow - deltaTooltipFromTooltipContent;
-                newTop = tipContElement[0].offsetTop + tipContElement[0].offsetHeight / 2 - tipElement[0].offsetHeight / 2;
+                  newLeft = theTipContElement.offsetLeft
+                    + theTipContClientRects[0].width / 2
+                    - theTipClientRects[0].width / 2;
+                  newTop = theTipContElement.offsetTop
+                    - marginLeftTooltipArrow
+                    - theTipClientRects[0].height
+                    - deltaTooltipFromTooltipContent;
+                } else if (attrs.tooltipSide === 'left') {
+
+                  for (clientRectIndex = 0; clientRectIndex < theTipContClientRects.length; clientRectIndex += 1) {
+
+                    aClientRect = theTipContClientRects.item(clientRectIndex);
+                    if (aClientRect &&
+                      aClientRect.left <= contentMinLeft) {
+
+                      contentMinLeft = aClientRect.left;
+                    }
+                  }
+
+                  newLeft = contentMinLeft
+                    - theTipElement.offsetWidth
+                    - marginLeftTooltipArrow
+                    - deltaTooltipFromTooltipContent;
+                  newTop = theTipContElement.offsetTop
+                    + theTipContElement.offsetHeight / 2
+                    - theTipElement.offsetHeight / 2;
+                } else if (attrs.tooltipSide === 'bottom') {
+
+                  newLeft = theTipContClientRects[theTipContClientRects.length - 1].left
+                    + theTipContClientRects[theTipContClientRects.length - 1].width / 2
+                    - theTipClientRects[0].width / 2;
+                  newTop = theTipContElement.offsetTop
+                    + theTipContElement.offsetHeight
+                    + marginLeftTooltipArrow;
+                } else if (attrs.tooltipSide === 'right') {
+
+                  for (clientRectIndex = 0; clientRectIndex < theTipContClientRects.length; clientRectIndex += 1) {
+
+                    aClientRect = theTipContClientRects.item(clientRectIndex);
+                    if (aClientRect &&
+                      aClientRect.left <= contentMinLeft) {
+
+                      contentMinLeft = aClientRect.left;
+                    }
+                  }
+
+                  newLeft = contentMinLeft
+                    + theTipContElement.offsetWidth
+                    + marginLeftTooltipArrow;
+                  newTop = theTipContElement.offsetTop
+                    + theTipContElement.offsetHeight / 2
+                    - theTipElement.offsetHeight / 2;
+                }
+                /*jshint +W014*/
               }
 
               tipElement.css({
                 'left': newLeft + 'px',
                 'top': newTop + 'px'
               });
-              /*if (attrs.tooltipSide === 'top' || attrs.tooltipSide === 'bottom') {
-
-                newLeft = -(actualWidth / 2 + actualPaddingLeft / 2);
-                tipElement.css('left', newLeft + 'px');
-              } else if (attrs.tooltipSide === 'left' || attrs.tooltipSide === 'right') {
-
-                newTop = -(actualHeight / 2 + actualPaddingTop / 2);
-                tipElement.css('top', newTop + 'px');
-              }*/
             }
           }
           , onTooltipHide = function onTooltipHide() {
 
             element.removeClass('active');
-          }
-          , onTooltipTitleChange = function onTooltipTitleChange(newValue, oldValue) {
-
-            if (newValue) {
-
-              if (oldValue) {
-
-                element.find('tip').html(newValue);
-              }
-              scope.tooltipCtrl.tooltipTitle = newValue;
-            }
           }
           , onTooltipTemplateChange = function onTooltipTemplateChange(newValue, oldValue) {
 
@@ -142,9 +226,9 @@
 
               if (oldValue) {
 
-                element.removeAttr(oldValue);
+                element.removeAttr('_' + oldValue);
               }
-              element.addClass(newValue);
+              element.addClass('_' + newValue);
               scope.tooltipCtrl.tooltipSide = newValue;
             }
           }
@@ -170,15 +254,17 @@
               element.on(newValue, onTooltipHide);
             }
           }
-          , unregisterOnTooltipTitleChangeObserver = attrs.$observe('tooltipTitle', onTooltipTitleChange)
           , unregisterOnTooltipTemplateChange = attrs.$observe('tooltipTemplate', onTooltipTemplateChange)
           , unregisterOnTooltipSideChangeObserver = attrs.$observe('tooltipSide', onTooltipSideChange)
           , unregisterOnTooltipShowTrigger = attrs.$observe('tooltipShowTrigger', onTooltipShowTrigger)
           , unregisterOnTooltipHideTrigger = attrs.$observe('tooltipHideTrigger', onTooltipHideTrigger);
 
+          resizeObserver.add(function registerResize() {
+
+            onTooltipShow(undefined);
+          });
           scope.$on('$destroy', function unregisterListeners() {
 
-            unregisterOnTooltipTitleChangeObserver();
             unregisterOnTooltipTemplateChange();
             unregisterOnTooltipSideChangeObserver();
             unregisterOnTooltipShowTrigger();
@@ -192,4 +278,4 @@
 
   angular.module('720kb.tooltips', [])
   .directive(directiveName, tooltipDirective);
-}(angular, document));
+}(angular, window));
