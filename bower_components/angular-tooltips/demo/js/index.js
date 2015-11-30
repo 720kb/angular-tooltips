84d4a06d458b5f1f8ca85443c26fb096672bd6e3
45kb <filippo.oretti@gmail.com>
2015-11-30 22:31:50 +0100

    gh with new module version - still a problem on display:inline-block;

---
+++ bower_components/angular-tooltips/demo/js/index.js
@@ -0,0 +1,56 @@
+/*global angular*/
+(function withAngular(angular) {
+  'use strict';
+
+  angular.module('720kb', [
+    '720kb.tooltips'
+  ])
+  .controller('DemoCtrl', [
+    '$timeout',
+    function controllerCtrl($timeout) {
+      var that = this;
+
+      that.tooltipContentText = 'I\'m a content text from module controller';
+      that.tooltipText = 'I\'m a text from module controller';
+      that.class = 'a-class';
+      that.closeFromModel = false;
+      that.sizeFromModel = 'small';
+      that.speedFromModel = 'fast';
+      $timeout(function changeTooltipText() {
+
+        that.tooltipText = 'C, C, C, Changed....';
+      }, 5000);
+      $timeout(function changeTooltipText() {
+
+        that.tooltipContentText = 'Content C, C, C, Changed....';
+      }, 6000);
+      $timeout(function changeTooltipText() {
+
+        that.closeFromModel = true;
+      }, 7000);
+      $timeout(function changeTooltipText() {
+
+        that.class = 'another-class';
+      }, 8000);
+      $timeout(function () {
+
+        that.sizeFromModel = 'large';
+      }, 9000);
+      $timeout(function () {
+
+        that.speedFromModel = 'slow';
+      }, 10000);
+
+
+      that.generateHTMLextra = function generateHTMLextra(item) {
+
+        return '<i>hello tooltip content' + item + '</i>';
+      };
+    }
+  ])
+  .controller('Ctrl', [function controller() {
+    var that = this;
+
+    that.items = [1, 2, 3, 4, 5];
+  }]);
+}(angular));
---
+++ bower_components/angular-tooltips/index.js
@@ -0,0 +1,8 @@
+/*global require,module*/
+(function commonJS(require, module) {
+  'use strict';
+
+  require('./src/lib/angular-tooltips');
+
+  module.exports = '720kb.tooltips';
+}(require, module));
