53bd5a3a5b14b4912dacf55117ee08004a7e7797
May Pongpitpitak <may.pognpitpitak@aggregateiq.com>
2016-05-10 15:13:47 -0700

    Export dist file instead of source.
    We shouldn't expect other projects to build angular-tootips since the
    build process is specific to the package like using annotation and so
    on.
    If this is required by a project that doesn't use annotation plugin, it
    will cause Angular to throw `strict di` error message. By exporting the
    dist file, it will be ready to use right away.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 (function commonJS(require, module) {
   'use strict';
 
-  require('./lib/angular-tooltips');
+  require('./dist/angular-tooltips');
 
   module.exports = '720kb.tooltips';
 }(require, module));
